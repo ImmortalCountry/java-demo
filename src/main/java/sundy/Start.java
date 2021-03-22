bf903bc964787d265c0b49e7d5c57a1e444ed079
sundy <sundy_u@163.com>
2021-03-22 16:06:26 +0800

    hello java

--- src/main/java/sundy/Start.java
+++ src/main/java/sundy/Start.java
@@ -2,9 +2,10 @@ package sundy;
 
 /**
  * @author sundy
+ * @date 2021/3/22 16:03
  */
 public class Start {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("hello Java!");
     }
 }
