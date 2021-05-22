e5edd34d9d12b31b2c09ec699e69fbd9c957a8c0
sundy <sundy_u@163.com>
2021-05-22 23:45:59 +0800

    commit

--- src/main/java/sundy/Start.java
+++ src/main/java/sundy/Start.java
@@ -1,27 +1,16 @@
 package sundy;
 
-import com.google.common.base.Strings;
-import com.google.common.collect.Maps;
-import org.apache.commons.lang3.StringUtils;
-import sundy.Utils.Bean;
-import sundy.Utils.BeanUtils;
-import sundy.Utils.enums.CodeStyleEnum;
-
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Properties;
 
 /**
  * @author sundy
  * @date 2021/3/22 16:03
  */
 public class Start {
-    public static void main(String[] args) throws InstantiationException, IllegalAccessException, InvocationTargetException {
-        Properties properties = System.getProperties();
-        for (Object obj : properties.keySet()) {
-            System.out.println("key:" + obj + "-->" + properties.get(obj));
-        }
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.stream();
     }
+
 }
--- src/main/java/sundy/annotation/People.java
+++ src/main/java/sundy/annotation/People.java
@@ -7,11 +7,15 @@ import lombok.Data;
  * @date 2021/3/23 17:03
  */
 @Data
+@ClassAnnotation
 public class People {
     @PeopleAnnotation("注解赋值名字")
     private String name;
 
     public static void main(String[] args) {
         System.out.println(PeopleAnnotationUtil.getFruitInfo(People.class));
+    }
+    public void print(){
+        System.out.println("hello");
     }
 }
